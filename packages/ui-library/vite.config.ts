fa8a7058aacbdf894ef5dc40a4b0b4bed868cc08
willcuii <william.cui@prenuvo.com>
2024-01-21 16:22:35 -0500

    initial structure setup

---
+++ packages/ui-library/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+    sourcemap: true,
+    target: 'es2020',
+  },
+  plugins: [react()],
+  publicDir: false,
+})
