25dfdf700385c27d8de6fd4b4cd876e0a23e5206
William Cui <william.cui@prenuvo.com>
2024-05-26 16:07:35 -0400

    Update Button components

--- packages/halo-web/src/button.tsx
+++ packages/halo-web/src/button.tsx
@@ -5,11 +5,10 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 export function Button({ children, ...other }: ButtonProps): JSX.Element {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <button type="button" {...other}>
+    <button type="button" {...other} className="test">
       {children}
     </button>
   );
 }
-
 
 Button.displayName = 'Button';
